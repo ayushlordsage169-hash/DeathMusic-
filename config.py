58d577b6c56311e46dccafeedc2aadd45e2c659f
ayushlordsage169-hash <ayushlordsage169@gmail.com>
2026-06-05 07:58:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -68,10 +68,10 @@ class Config:
         self.COOKIES_URL: List[str] = self._parse_cookies()
 
         # Images
-        self.DEFAULT_THUMB: str = getenv("DEFAULT_THUMB", "https://files.catbox.moe/zlmv6v.png")
-        self.PING_IMG: str = getenv("PING_IMG", "https://files.catbox.moe/zlmv6v.png")
-        self.START_IMG: str = getenv("START_IMG", "https://files.catbox.moe/zlmv6v.png")
-        self.RADIO_IMG: str = getenv("RADIO_IMG", "https://files.catbox.moe/zlmv6v.png")
+        self.DEFAULT_THUMB: str = getenv("DEFAULT_THUMB", "https://t.me/SAGEBOTMATERIAL/20")
+        self.PING_IMG: str = getenv("PING_IMG", "https://t.me/SAGEBOTMATERIAL/20")
+        self.START_IMG: str = getenv("START_IMG", "https://t.me/SAGEBOTMATERIAL/20")
+        self.RADIO_IMG: str = getenv("RADIO_IMG", "https://t.me/SAGEBOTMATERIAL/20")
 
         # Moderation
         self.EXCLUDED_USERNAMES: List[str] = getenv("EXCLUDED_USERNAMES", "").split()
